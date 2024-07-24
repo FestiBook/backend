2223d597f9a73356b28cb4312bed547b2e5ef8a5
fnzl54 <rnjscksdud1@naver.com>
2024-07-24 22:44:32 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/festibook/festibook_backend/FestibookBackendApplication.java
@@ -0,0 +1,13 @@
+package com.festibook.festibook_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestibookBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestibookBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/festibook/festibook_backend/FestibookBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.festibook.festibook_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestibookBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
