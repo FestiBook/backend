78e8ed7936d6c0e88c9d9f7ea656d047107c1836
kanguk <lm0828@naver.com>
2024-08-16 16:46:06 +0900

    feat:(4) 카카오, 네이버, 구글 로그인을 통해 유저 회원가입 및 로그인을 할 수 있다.

---
+++ src/main/java/com/festibook/festibook_backend/jwt/TokenService.java
@@ -0,0 +1,105 @@
+package com.festibook.festibook_backend.jwt;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.UUID;
+
+@Slf4j
+@Service
+public class TokenService {
+    private Key secretKey;
+
+    @Value("${jwt.token.access-expire-length}")
+    private Long ACCESS_EXPIRE_LENGTH;
+
+    @Value("${jwt.token.refresh-expire-length}")
+    private Long REFRESH_EXPIRE_LENGTH;
+
+    @PostConstruct
+    protected void init() {
+        this.secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256); // 자동으로 강력한 키를 생성합니다.
+    }
+
+    public String generateAccessToken(UUID userId) {
+        Claims claims = Jwts.claims().setSubject(userId.toString());
+        return Jwts.builder().setClaims(claims)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + ACCESS_EXPIRE_LENGTH))
+                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String generateRefreshToken() {
+        return Jwts.builder()
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + REFRESH_EXPIRE_LENGTH))
+                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String reGenerateAccessToken(HttpServletRequest request) { // 액세스 토큰 재발급
+        validateRefreshToken(request);
+        UUID id = getUserIdFromToken(request);
+        return generateAccessToken(id);
+    }
+
+    public String resolveAccessToken(HttpServletRequest request) {
+        try {
+            String header = request.getHeader("AUTHORIZATION");
+            return header.substring("Bearer ".length());
+        } catch (Exception e) {
+            throw new JwtException("유효하지 않은 토큰");
+        }
+    }
+
+    public String resolveRefreshToken(HttpServletRequest request) {
+        try {
+            return request.getHeader("REFRESH-TOKEN");
+        } catch (Exception e) {
+            throw new JwtException("유효하지 않은 토큰");
+        }
+    }
+
+    public void validateAccessToken(HttpServletRequest request) { // 만료 여부 검사
+        try {
+            String token = resolveAccessToken(request);
+            Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token);
+        } catch (ExpiredJwtException e) {
+            throw new JwtException("토큰 만료");
+        }
+    }
+
+    public void validateRefreshToken(HttpServletRequest request) {
+        try {
+            String token = resolveRefreshToken(request);
+            Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token);
+        } catch (ExpiredJwtException e) { // 토큰이 만료된 경우
+            throw new JwtException("토큰 만료");
+        } catch (IllegalArgumentException e) { // 토큰이 비어있거나 형식이 잘못된 경우
+            throw new JwtException("유효하지 않은 토큰");
+        }
+    }
+
+    public UUID getUserIdFromToken(HttpServletRequest request) { // 토큰에서 userId 정보 꺼내기
+        String token = resolveAccessToken(request);
+        try {
+            Claims claims = Jwts.parserBuilder()
+                    .setSigningKey(secretKey)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+            return UUID.fromString(claims.getSubject());
+        } catch (ExpiredJwtException e) {
+            return UUID.fromString(e.getClaims().getSubject());
+        }
+    }
+}
---
+++ src/main/java/com/festibook/festibook_backend/oauth2/user/OAuth2Attributes.java
@@ -0,0 +1,54 @@
+package com.festibook.festibook_backend.oauth2.user;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
+@Builder
+public class OAuth2Attributes {
+    private Map<String, Object> attributes;
+    private String nameAttributeKey;
+    private String name;
+
+    public static OAuth2Attributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        } else if ("kakao".equals(registrationId)) {
+            return ofKakao("id", attributes);
+        }
+        return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    private static OAuth2Attributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
+        return OAuth2Attributes.builder()
+                .name((String) attributes.get("name"))
+                .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    private static OAuth2Attributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuth2Attributes.builder()
+                .name((String) response.get("nickname"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    private static OAuth2Attributes ofKakao(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("kakao_account");
+        Map<String, Object> profile = (Map<String, Object>) response.get("profile");
+
+        response.put("id", attributes.get("id"));
+
+        return OAuth2Attributes.builder()
+                .name((String) profile.get("nickname"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+}
