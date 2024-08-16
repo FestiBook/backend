c9c3d88094090181dcf6e8f18c7da83b3f4bc46d
kanguk <lm0828@naver.com>
2024-08-16 16:46:06 +0900

    feat:(4) 카카오, 네이버, 구글 로그인을 통해 유저 회원가입 및 로그인을 할 수 있다.

---
+++ src/main/java/com/festibook/festibook_backend/oauth2/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package com.festibook.festibook_backend.oauth2.config;
+
+import com.festibook.festibook_backend.oauth2.handler.OAuth2FailureHandler;
+import com.festibook.festibook_backend.oauth2.handler.OAuth2SuccessHandler;
+import com.festibook.festibook_backend.oauth2.service.CustomOAuth2UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final OAuth2SuccessHandler oAuth2SuccessHandler;
+    private final OAuth2FailureHandler oAuth2FailureHandler;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(
+                        AbstractHttpConfigurer::disable
+                )
+                .authorizeHttpRequests(requests ->
+                        requests.anyRequest().permitAll()
+                )
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .oauth2Login(oauth2Login -> oauth2Login
+                        .userInfoEndpoint(userInfoEndpoint -> userInfoEndpoint
+                                .userService(customOAuth2UserService)
+                        )
+                        .successHandler(oAuth2SuccessHandler)
+                        .failureHandler(oAuth2FailureHandler)
+                );
+
+        return http.build();
+    }
+}
