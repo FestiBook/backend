5f5032022134be16ea37b606a7d906144483f4dd
kanguk <lm0828@naver.com>
2024-08-16 15:22:22 +0900

    feat:(4) 로그인을 구현하기 위해 User 엔티티를 설정한다.

---
+++ src/main/java/com/festibook/festibook_backend/user/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.festibook.festibook_backend.user.repository;
+
+import com.festibook.festibook_backend.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByNickname(String nickname);
+}
