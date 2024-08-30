0a9e8f92ed80f273f149287d43ea13ad289103c2
fnzl54 <rnjscksdud1@naver.com>
2024-08-30 23:46:55 +0900

    feat:(3) 스프링 배치를 통해 이벤트를 주기적으로 업데이트 할 수 있다.

--- src/main/java/com/festibook/festibook_backend/FestibookBackendApplication.java
+++ src/main/java/com/festibook/festibook_backend/FestibookBackendApplication.java
@@ -2,8 +2,10 @@ package com.festibook.festibook_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableScheduling
 @SpringBootApplication
 @EnableJpaAuditing
 public class FestibookBackendApplication {
---
+++ src/main/java/com/festibook/festibook_backend/event/batch/CacheEventJob.java
@@ -0,0 +1,79 @@
+package com.festibook.festibook_backend.event.batch;
+
+
+import com.festibook.festibook_backend.event.entity.Event;
+import com.festibook.festibook_backend.event.repository.EventRepository;
+import com.festibook.festibook_backend.event.useCase.CacheEventUseCase;
+import com.festibook.festibook_backend.event.useCase.CacheEventUseCase.CacheEventRequest;
+import com.festibook.festibook_backend.event.useCase.CacheEventUseCase.CacheEventResponse;
+import com.festibook.festibook_backend.external.event.dto.EventResponse.Item;
+import jakarta.annotation.PostConstruct;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@Log4j2
+@RequiredArgsConstructor
+@Configuration
+public class CacheEventJob {
+
+  private final CacheEventUseCase cacheEventUseCase;
+
+  private final EventRepository eventRepository;
+
+  @PostConstruct
+  @Scheduled(cron = "0 0 1 * * *")
+  public void CacheEventJobScheduler() {
+    LocalDate currentDate = LocalDate.now();
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+    String formattedDate = currentDate.format(formatter);
+
+    CacheEventResponse cacheEventResponse = cacheEventUseCase.execute(CacheEventRequest.builder()
+        .eventStartDate(formattedDate)
+        .build());
+
+    List<Item> items = cacheEventResponse.getItems();
+    List<Event> events = new ArrayList<>();
+    for (Item item : items) {
+      Optional<Event> existingEvent = eventRepository.findByContentId(item.getContentid());
+      if (existingEvent.isPresent()) {
+        Event eventToUpdate = existingEvent.get();
+        updateEvent(eventToUpdate, item);
+        events.add(eventToUpdate);
+      } else {
+        events.add(createEvent(item));
+      }
+    }
+
+    eventRepository.saveAll(events);
+  }
+
+  private void updateEvent(Event event, Item item) {
+    event.setTitle(item.getTitle());
+    event.setAddress1(item.getAddr1());
+    event.setAddress2(item.getAddr2());
+    event.setOriginUrl(item.getFirstimage());
+    event.setThumbnailUrl(item.getFirstimage2());
+    event.setMapX(item.getMapx());
+    event.setMapY(item.getMapy());
+  }
+
+  private Event createEvent(Item item) {
+    return Event.builder()
+        .contentId(item.getContentid())
+        .title(item.getTitle())
+        .address1(item.getAddr1())
+        .address2(item.getAddr2())
+        .originUrl(item.getFirstimage())
+        .thumbnailUrl(item.getFirstimage2())
+        .mapX(item.getMapx())
+        .mapY(item.getMapy())
+        .build();
+  }
+}
