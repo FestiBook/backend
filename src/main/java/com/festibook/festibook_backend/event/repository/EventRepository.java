5df5ac7ce65eb908f2b6f5d43d06d9ba5be20458
kanguk <lm0828@naver.com>
2024-08-30 20:26:58 +0900

    fix:(4) rebase 중 잘못 포함된 Event 관련 내용 삭제한다.

--- src/main/java/com/festibook/festibook_backend/event/repository/EventRepository.java
+++
@@ -1,8 +0,0 @@
-package com.festibook.festibook_backend.event.repository;
-
-import com.festibook.festibook_backend.event.entity.Event;
-import java.util.UUID;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventRepository extends JpaRepository<Event, UUID> {
-}
