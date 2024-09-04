b3b334e6af7aea40be98c2f1f53beff637c1b9dd
kanguk <lm0828@naver.com>
2024-09-04 23:31:03 +0900

    feat:(8) 리뷰 기능을 추가한다. (CRUD)

---
+++ src/main/java/com/festibook/festibook_backend/review/dto/ReviewRequestDto.java
@@ -0,0 +1,12 @@
+package com.festibook.festibook_backend.review.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ReviewRequestDto {
+    private String content;
+}
