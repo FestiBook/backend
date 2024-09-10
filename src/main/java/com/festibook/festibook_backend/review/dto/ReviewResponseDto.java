ba13c0ba8d16b16a8a4aba446136a7e60b37e769
fnzl54 <rnjscksdud1@naver.com>
2024-09-11 00:33:02 +0900

    feat:(17) 리뷰 api의 response에 이벤트 id, 이름, 리뷰 생성 날짜를 추가한다.

--- src/main/java/com/festibook/festibook_backend/review/dto/ReviewResponseDto.java
+++ src/main/java/com/festibook/festibook_backend/review/dto/ReviewResponseDto.java
@@ -3,12 +3,17 @@ package com.festibook.festibook_backend.review.dto;
 import com.festibook.festibook_backend.review.entity.Review;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class ReviewResponseDto {
     private Long id;
     private Long userId;
     private String nickname;
     private String content;
+    private Long eventId;
+    private String eventName;
+    private LocalDateTime createdAt;
 
     public static ReviewResponseDto fromEntity(Review review) {
         ReviewResponseDto dto = new ReviewResponseDto();
@@ -16,6 +21,9 @@ public class ReviewResponseDto {
         dto.userId = review.getUser().getId();
         dto.nickname = review.getUser().getNickname();
         dto.content = review.getContent();
+        dto.eventId = review.getEvent().getId();
+        dto.eventName = review.getEvent().getTitle();
+        dto.createdAt = review.getCreatedAt();
         return dto;
     }
 }
